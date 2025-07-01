3d303227a4c9b16460b7e496e4416f5e87ea96ef
HerreraHernandezJovannaJatziry <20223tn062@utez.edu.mx>
2025-07-01 16:16:59 -0600

    controller

--- unidad3/src/main/java/utez/edu/mx/unidad3/modules/cede/CedeController.java
+++ unidad3/src/main/java/utez/edu/mx/unidad3/modules/cede/CedeController.java
@@ -17,9 +17,27 @@ public class CedeController {
         APIResponse response=cedeService.findAll();
         return new ResponseEntity<>(response,response.getStatus());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<APIResponse> findById(@PathVariable("id")Long id){
+        APIResponse response=cedeService.findById(id);
+        return new ResponseEntity<>(response,response.getStatus());
+    }
+
     @PostMapping("")
     public ResponseEntity<APIResponse> save(@RequestBody Cede payload){
         APIResponse response=cedeService.save(payload);
+        return new ResponseEntity<>(response,response.getStatus());
+    }
+
+    @PutMapping("")
+    public ResponseEntity<APIResponse>update(@RequestBody Cede payload){
+        APIResponse response=cedeService.update(payload);
+        return new ResponseEntity<>(response,response.getStatus());
+    }
+    @DeleteMapping("")
+    public ResponseEntity<APIResponse>remove(@RequestBody Cede payload){
+        APIResponse response=cedeService.remove(payload);
         return new ResponseEntity<>(response,response.getStatus());
     }
 }
